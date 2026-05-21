52e6fadad7b5b0d2cf59c246a404d808f3469c20
LoQ <LoQ@10.44.9.45>
2026-05-22 00:03:49 +0700

    Merge branch 'master' of https://github.com/Fajriaulll/PrakSD2026_A_2511532009.git

--- PrakSD2026_A_2511532009/src/pekan7_2511532009/BubbleSort_2511532009.java
+++ PrakSD2026_A_2511532009/src/pekan7_2511532009/BubbleSort_2511532009.java
@@ -14,7 +14,7 @@ public class BubbleSort_2511532009 {
             }
         }
     }
-
+    
     public static void main(String[] args) {
         int arr_2009[] = { 23, 78, 45, 8, 32, 56, 1 };
         int n_2009 = arr_2009.length;
